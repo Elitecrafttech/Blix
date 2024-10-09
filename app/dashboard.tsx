a1c7298d2162d21925316786ea46827880437337
Chrismiweb <chrismibiteso@gmail.com>
2024-10-09 02:23:15 -0700

    installed native wind

---
+++ app/dashboard.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const dashboard = () => {
+  return (
+    <View>
+      <Text>
+        hello world
+      </Text>
+    </View>
+  )
+}
+
+export default dashboard
---
+++ components/App.tsx
@@ -0,0 +1,15 @@
+import { ThemeProvider } from '@react-navigation/native'
+import { Stack } from 'expo-router'
+import React from 'react'
+import { SafeAreaView, View, Text, ScrollView } from 'react-native'
+
+const App = () => {
+  return (
+    <ScrollView>
+      
+    </ScrollView>
+      
+  )
+}
+
+export default App
