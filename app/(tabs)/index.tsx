cd2d23e88961437298148d00e4c7c0df0bae3843
Chrismiweb <chrismibiteso@gmail.com>
2024-10-15 18:41:45 -0700

    wallet page design

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,10 @@
 import { AppContext } from '@/context/AppContext'
 import React, { useContext } from 'react'
 import Onboarding from '../onbording';
-// import Dashboard from '../dashboard';
+import Dashboard from '../dashboard';
 
 export default function index() {
   const {isAuthenticated} = useContext(AppContext);
 
-  return isAuthenticated ? "" : <Onboarding />;
+  return isAuthenticated ? "" : <Dashboard />;
 }
